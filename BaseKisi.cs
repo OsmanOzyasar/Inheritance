8bb54dcbb8f03400fea96342dd49fa8168e69b06
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-08 16:31:54 +0300

    Add project files.

---
+++ BaseKisi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public abstract class BaseKisi
+    {
+        public string? Name {  get; set; }
+        public string? Surname { get; set; }
+
+        public void WriteName()
+        {
+            Console.WriteLine($"İsim: {Name} \nSoyisim: {Surname}");
+        }
+    }
+
+    public class Teacher : BaseKisi
+    {
+        public int Salary { get; set; }
+        public void WriteSalary ()
+        {
+            Console.WriteLine("Öğretmen:");
+            WriteName();
+            Console.WriteLine($"Maaş miktarı: {Salary} TL \n");          
+        }
+    }
+
+    public class Student : BaseKisi
+    {
+        public int StudentNumber { get; set; } 
+        public void WriteNumber()
+        {
+            Console.WriteLine("Öğrenci:");
+            WriteName();
+            Console.WriteLine($"Öğrenci numarası: {StudentNumber} \n");           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student()
+            {
+                Name = "Osman",
+                Surname = "Özyaşar",
+                StudentNumber = 159
+            };
+
+            student.WriteNumber();
+
+            Teacher teacher = new Teacher()
+            {
+                Name = "Ökkeş",
+                Surname = "Gılıbık",
+                Salary = 50000
+            };
+
+            teacher.WriteSalary();
+            
+        }
+    }
+}
